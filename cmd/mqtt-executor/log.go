2bffef9f6c289fdef74abc3c174f768ff977f6a9
rainu <rainu@raysha.de>
2020-05-06 18:31:12 +0200

    dont print stacktraces

--- cmd/mqtt-executor/log.go
+++ cmd/mqtt-executor/log.go
@@ -3,7 +3,9 @@ package main
 import "go.uber.org/zap"
 
 func init() {
-	logger, _ := zap.NewDevelopment()
+	logger, _ := zap.NewDevelopment(
+		zap.AddStacktrace(zap.FatalLevel),
+	)
 	zap.ReplaceGlobals(logger)
 	defer zap.L().Sync()
 }
