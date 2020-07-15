d8d5bce4fa1e7fe5d96b6a6131162ea5dee31faa
rainu <rainu@raysha.de>
2020-07-15 16:33:34 +0200

    add example shell script for gather some system stats

---
+++ scripts/systemStats.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+MEMORY_STATS=$(cat /proc/meminfo | sed 's/://g' | awk '{ print "\""$1"\":{\"value\":"$2",\"unit\":\""$3"\"},"}' | tr -d '\n' | sed 's/,$//')
+CPU_STATS=$(mpstat -P ALL | grep "^[0-9]" | grep -v "CPU" | sed 's/,/./g' | awk '{print "\""$2"\":{\"%usr\":"$3",\"%nice\":"$4",\"%sys\":"$5",\"%iowait\":"$6",\"%irq\":"$7",\"%soft\":"$8",\"%steal\":"$9",\"%guest\":"$10",\"%idle\":"$11"},"}' | tr -d '\n' | sed 's/,$//')
+IO_STATS=$(iostat -d -k | grep "[0-9]$" | sed 's/,/./g' | awk '{print "\""$1"\":{\"tps\":"$2",\"read/s\":"$3",\"wrtn/s\":"$4",\"read\":"$5",\"wrtn\":"$6"},"}' | tr -d '\n' | sed 's/,$//')
+DISK_STATS=$(df -P | grep -v "^tmpfs" | grep -v "^shm" | grep -v "^run" | grep -v "^dev" | sed 1d | awk '{print "\""$6"\":{\"fs\":\""$1"\",\"used\":"$3",\"free\":"$4",\"total\":"$3+$4"},"}' | tr -d '\n' | sed 's/,$//')
+NETWORK_STATS=$(ifstat -j | sed 's/^{//' | sed 's/}$//')
+
+read -d '' JSON << EOF
+{
+    "timestamp": $(date +%s),
+    "mem": {
+        ${MEMORY_STATS}
+    },
+    "cpu": {
+        ${CPU_STATS}
+    },
+    "io": {
+        ${IO_STATS}
+    },
+    "disk": {
+        ${DISK_STATS}
+    },
+    "net": {
+        ${NETWORK_STATS}
+    }
+}
+EOF
+
+echo ${JSON} | jq -c
