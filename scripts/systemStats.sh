06ca581e42e7e69ed6ae2522073c383359419316
rainu <rainu@raysha.de>
2020-08-03 17:18:16 +0200

    use top if possible

--- scripts/systemStats.sh
+++ scripts/systemStats.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 MEMORY_STATS=$(cat /proc/meminfo | sed 's/://g' | awk '{ print "\""$1"\":{\"value\":"$2",\"unit\":\""$3"\"},"}' | tr -d '\n' | sed 's/,$//')
-CPU_STATS=$(mpstat -P ALL | grep "^[0-9]" | grep -v "CPU" | sed 's/,/./g' | awk '{print "\""$2"\":{\"%usr\":"$3",\"%nice\":"$4",\"%sys\":"$5",\"%iowait\":"$6",\"%irq\":"$7",\"%soft\":"$8",\"%steal\":"$9",\"%guest\":"$10",\"%idle\":"$11"},"}' | tr -d '\n' | sed 's/,$//')
 IO_STATS=$(iostat -d -k | grep "[0-9]$" | sed 's/,/./g' | awk '{print "\""$1"\":{\"tps\":"$2",\"read/s\":"$3",\"wrtn/s\":"$4",\"read\":"$5",\"wrtn\":"$6"},"}' | tr -d '\n' | sed 's/,$//')
 DISK_STATS=$(df -P | grep -v "^tmpfs" | grep -v "^shm" | grep -v "^run" | grep -v "^dev" | sed 1d | awk '{print "\""$6"\":{\"fs\":\""$1"\",\"used\":"$3",\"free\":"$4",\"total\":"$3+$4"},"}' | tr -d '\n' | sed 's/,$//')
 NETWORK_STATS=$(ifstat -j | sed 's/^{//' | sed 's/}$//')
 LOAD_AVG=$(uptime | grep -o "load average: .*$" | cut -d\: -f2 | sed 's/, / /g' | sed 's/,/./g' | awk '{print $1", "$2", "$3}')
+
+TOP_OUT=$(echo "1" | top -n1 2> /dev/null | grep "^CPU" | sed 's/CPU//g' | sed 's/\%//g' | awk '{print "\""$1"\":{\"%usr\":"$2",\"%sys\":"$4",\"%nice\":"$6",\"%idle\":"$8",\"%iowait\":"$10",\"%irq\":"$12",\"%soft\":"$14"},"}' | tr -d '\n')
+
+if [ "$TOP_OUT" != "" ]; then
+  CPU_ALL=$(top -n1  | grep "^CPU" | sed 's/CPU//g' | sed 's/\%//g' | awk '{print "\"all\":{\"%usr\":"$2",\"%sys\":"$4",\"%nice\":"$6",\"%idle\":"$8",\"%iowait\":"$10",\"%irq\":"$12",\"%soft\":"$14"}"}'  | tr -d '\n' | sed 's/,$//')
+  CPU_STATS=${TOP_OUT}${CPU_ALL}
+else
+  CPU_STATS=$(mpstat -P ALL | grep "^[0-9]" | grep -v "CPU" | sed 's/,/./g' | awk '{print "\""$2"\":{\"%usr\":"$3",\"%nice\":"$4",\"%sys\":"$5",\"%iowait\":"$6",\"%irq\":"$7",\"%soft\":"$8",\"%steal\":"$9",\"%guest\":"$10",\"%idle\":"$11"},"}' | tr -d '\n' | sed 's/,$//')
+fi
 
 read -d '' JSON << EOF
 {
